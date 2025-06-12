314bafd8e4b45a747f449c38223db70bc3399c09
LJNIC <liamreadsspam@gmail.com>
2025-06-12 07:57:45 -0300

    Give things names

--- modules/MyGame/actors/kobold.lua
+++ modules/MyGame/actors/kobold.lua
@@ -1,11 +1,12 @@
 prism.registerActor("Kobold", function()
    return prism.Actor.fromComponents {
+      prism.components.Name("Kobold"),
       prism.components.Position(),
       prism.components.Collider(),
       prism.components.Drawable("k", prism.Color4.RED),
       prism.components.Senses(),
-      prism.components.Sight{ range = 12, fov = true },
-      prism.components.Mover{ "walk" },
-      prism.components.KoboldController()
+      prism.components.Sight { range = 12, fov = true },
+      prism.components.Mover { "walk" },
+      prism.components.KoboldController(),
    }
 end)
