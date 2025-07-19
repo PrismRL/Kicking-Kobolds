8debffc384cc6d81a26cf6974ef049fa946a4062
LJNIC <liamreadsspam@gmail.com>
2025-07-18 21:07:12 -0300

    Reverse floor & pit backgrounds

--- modules/game/cells/floor.lua
+++ modules/game/cells/floor.lua
@@ -1,6 +1,6 @@
 prism.registerCell("Floor", function()
    return prism.Cell.fromComponents {
-      prism.components.Drawable("."),
+      prism.components.Drawable(".", nil, prism.Color4.WHITE * 0.25),
       prism.components.Collider({ allowedMovetypes = { "walk", "fly" } }),
    }
 end)
