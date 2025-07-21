11d95db26b15ddaa8b91679ff03b99ce1d65eaba
LJNIC <liamreadsspam@gmail.com>
2025-07-21 08:56:51 -0300

    Update readme and pass actor to all wait actions

--- modules/game/components/koboldcontroller.lua
+++ modules/game/components/koboldcontroller.lua
@@ -5,29 +5,26 @@ KoboldController.name = "KoboldController"
 
 function KoboldController:act(level, actor)
    local senses = actor:get(prism.components.Senses)
-   if not senses then return prism.actions.Wait() end -- we can't see!
+   if not senses then return prism.actions.Wait(actor) end -- we can't see!
 
    local player = senses:query(prism.components.PlayerController):first()
-   if not player then return prism.actions.Wait() end -- no player nearby!
+   if not player then return prism.actions.Wait(actor) end -- no player nearby!
 
    local mover = actor:get(prism.components.Mover)
-   if not mover then return prism.actions.Wait() end -- we can't move!
+   if not mover then return prism.actions.Wait(actor) end -- we can't move!
 
    local path = level:findPath(actor:getPosition(), player:getPosition(), actor, mover.mask, 1)
 
    if path then
       local move = prism.actions.Move(actor, path:pop())
-      if level:canPerform(move) then
-         return move
-      end
+      if level:canPerform(move) then return move end
    end
 
    local attack = prism.actions.Attack(actor, player)
-   if level:canPerform(attack) then
-      level:perform(attack)
-   end
+   if level:canPerform(attack) then level:perform(attack) end
 
    return prism.actions.Wait(actor)
 end
 
 return KoboldController
+
