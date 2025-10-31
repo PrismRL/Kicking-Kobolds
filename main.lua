9077c0e68a3c5883b8cb554eb76a34aecec0258e
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-10-31 19:57:51 -0400

    Now correctly loads the equipment module.

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ prism.loadModule("prism/extra/log")
 prism.loadModule("prism/extra/inventory")
 prism.loadModule("prism/extra/droptable")
 prism.loadModule("prism/extra/statuseffects")
+prism.loadModule("prism/extra/equipment")
 prism.loadModule("modules/base")
 prism.loadModule("modules/game")
 
